4513fbd939bbfca85908130183abcc56be1fcd26
bwrosen20 <bwrosen20@gmail.com>
2023-06-16 13:21:42 -0400

    commit tree 20343cb94268ff9fa005909b5ba67b6c625bd2fc

---
+++ app/controllers/instructor_controller.rb
@@ -0,0 +1,40 @@
+class InstructorController < ApplicationController
+
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_error
+rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_error
+
+    def index
+        instructors = Instructor.all
+        render json: instructors
+    end
+
+    def destroy
+        instructor = Instructor.find(params[:id])
+        instructor.destroy
+        render json: instructor
+    end
+
+    def update 
+        instructor = Instructor.find(params[:id])
+        instructor.update(instructor_params)
+    end
+
+    def create
+        instructor = Instructor.create(instructor_params)
+        render json: instructor, status: :created
+    end
+
+    private
+
+    def instructor_params
+        params.permit(:name)
+    end
+
+    def render_not_found_error
+        render json: {error: "Instructor not found"}, status: :not_found
+    end
+
+    def render_unprocessable_entity_error(invalid)
+        render json: {errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
+    end
+end
